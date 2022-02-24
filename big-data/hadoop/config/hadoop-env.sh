32ee4d37e9e8c0499ea65609b1181ecc5967beef
icefery <icefery@163.com>
2022-02-24 18:53:54 +0800

    hadoop config

---
+++ big-data/hadoop/config/hadoop-env.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/opt/jdk
+export HADOOP_HOME=/opt/hadoop
+export HDFS_NAMENODE_USER=root
+export HDFS_SECONDARYNAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export YARN_RESOURCEMANAGER_USER=root
+export YARN_NODEMANAGER_USER=root
