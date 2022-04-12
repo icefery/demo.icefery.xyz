0da990370b95ed0a6f653dcd61b7427d83560b22
icefery <icefery@163.com>
2022-04-12 11:43:53 +0800

    [util] 工具模块更改

--- lang/java/util/src/main/java/xyz/icefery/demo/util/sql/NativeBatchExecutor.java
+++ lang/java/util/src/main/java/xyz/icefery/demo/util/sql/NativeBatchExecutor.java
@@ -3,21 +3,21 @@ package xyz.icefery.demo.util.sql;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import xyz.icefery.demo.util.function.ThrowableRunnable;
+import xyz.icefery.demo.util.function.ThrowableConsumer;
+import xyz.icefery.demo.util.function.ThrowableFunction;
+import xyz.icefery.demo.util.function.ThrowableSupplier;
 import xyz.icefery.demo.util.thread.AsyncAwait;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.function.Supplier;
 
 public class NativeBatchExecutor {
     private static final Logger log = LoggerFactory.getLogger(NativeBatchExecutor.class);
 
-    public static <T> ThrowableRunnable<Exception> batchInsert(Supplier<Connection> connectionGetter, ThreadPoolExecutor executor, String dialect, Class<T> cls, List<T> data, int batchSize) throws Exception {
+    public static <T> ThrowableConsumer<Boolean, Exception> batchInsert(ThreadPoolExecutor executor, ThrowableSupplier<Connection, Exception> connectionGetter, ThrowableFunction<List<T>, String, Exception> sqlGetter, List<T> data, int batchSize) throws Exception {
         // 开始时间
         long start = System.currentTimeMillis();
         // 分批
@@ -35,45 +35,38 @@ public class NativeBatchExecutor {
             // 当前批次编号
             String batchId = String.valueOf(i);
             // 当前批次语句
-            String batchSQL = NativeSQLBuilder.buildInsert(dialect, cls, batch);
+            String batchSQL = sqlGetter.apply(batch);
             // 添加任务
             tasks.add(() -> {
-                executor.execute(() -> {
-                    long batchStart = System.currentTimeMillis();
-                    String batchStatus;
-                    try (Statement statement = connection.createStatement()) {
-                        statement.execute(batchSQL);
-                        batchStatus = "success";
-                    } catch (Exception e) {
-                        exceptionCollector.put(batchId, e.getMessage());
-                        batchStatus = "failure";
-                    }
-                    long batchEnd = System.currentTimeMillis();
-                    log.info("total={} batchSize={} batchCount={} batchId={} batchStatus={} batchCost={} batchSQL={}", data.size(), batchSize, batches.size(), batchId, batchStatus, batchEnd - batchStart, batchSQL);
-                });
+                long batchStart = System.currentTimeMillis();
+                String batchStatus;
+                try (Statement statement = connection.createStatement()) {
+                    statement.execute(batchSQL);
+                    batchStatus = "success";
+                } catch (Exception e) {
+                    exceptionCollector.put(batchId, e.getMessage());
+                    batchStatus = "failure";
+                }
+                long batchEnd = System.currentTimeMillis();
+                log.info("total={} batchSize={} batchCount={} batchId={} batchStatus={} batchCost={} batchSQL={}", data.size(), batchSize, batches.size(), batchId, batchStatus, batchEnd - batchStart, batchSQL);
             });
         }
         // 执行任务
-        AsyncAwait.awaitAll(tasks);
+        AsyncAwait.awaitAll(executor, tasks);
         // 结束时间
         long end = System.currentTimeMillis();
         // 提交任务
-        return () -> {
+        return (commitable) -> {
             String txStatus;
-            if (exceptionCollector.isEmpty()) {
+            if (commitable && exceptionCollector.isEmpty()) {
                 connection.commit();
-                connection.close();
                 txStatus = "commit";
             } else {
                 connection.rollback();
-                connection.close();
                 txStatus = "rollback";
             }
+            connection.close();
             log.info("txStatus={} cost={}", txStatus, end - start);
-            // 回滚时抛出异常以影响外部事务
-            if (Objects.equals(txStatus, "rollback")) {
-                throw new RuntimeException();
-            }
         };
     }
 }
--- lang/java/util/src/main/java/xyz/icefery/demo/util/thread/AsyncAwait.java
+++ lang/java/util/src/main/java/xyz/icefery/demo/util/thread/AsyncAwait.java
@@ -2,16 +2,19 @@ package xyz.icefery.demo.util.thread;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class AsyncAwait {
-    public static void awaitAll(List<Runnable> tasks) throws Exception {
+    public static void awaitAll(ThreadPoolExecutor pool, List<Runnable> tasks) throws Exception {
         CountDownLatch countDownLatch = new CountDownLatch(tasks.size());
         for (Runnable task : tasks) {
-            try {
-                task.run();
-            } finally {
-                countDownLatch.countDown();
-            }
+            pool.execute(() -> {
+                try {
+                    task.run();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
         }
         countDownLatch.await();
     }
