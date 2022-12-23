b8bd2f262abad3ca71536301cdc8319087b73857
icefery <icefery@163.com>
2022-12-23 16:09:06 +0800

    :pencil: 更新文档

--- root/build-tool/gradle/__static__/init.gradle.kts
+++ root/build-tool/gradle/__static__/init.gradle.kts
@@ -10,7 +10,7 @@ initscript {
     }
 }
 
-buildscript {
+allprojects {
     repositories {
         mavenLocal()
         maven("https://maven.aliyun.com/nexus/content/groups/public")
@@ -20,16 +20,15 @@ buildscript {
         maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
         mavenCentral()
     }
-}
-
-allprojects {
-    repositories{
-        mavenLocal()
-        maven("https://maven.aliyun.com/nexus/content/groups/public")
-        maven("https://maven.aliyun.com/nexus/content/repositories/spring")
-        maven("https://maven.aliyun.com/nexus/content/repositories/spring-plugin")
-        maven("https://maven.aliyun.com/nexus/content/repositories/google")
-        maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
-        mavenCentral()
+    buildscript {
+        repositories {
+            mavenLocal()
+            maven("https://maven.aliyun.com/nexus/content/groups/public")
+            maven("https://maven.aliyun.com/nexus/content/repositories/spring")
+            maven("https://maven.aliyun.com/nexus/content/repositories/spring-plugin")
+            maven("https://maven.aliyun.com/nexus/content/repositories/google")
+            maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+            mavenCentral()
+        }
     }
 }
--- root/db/mysql/README.md
+++ root/db/mysql/README.md
@@ -1,1 +1,5 @@
 # MySQL
+
+## 收藏
+
+#### [mysql 设置了 utf8mb4，为什么还有 utf8mb4_general_ci 和 utf8mb4_0900_ai_ci？](https://www.cnblogs.com/seasonhu/p/14994857.html)
